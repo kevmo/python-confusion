39e49cca0b6ea345ba276d250b99bd31325be948
Kevin Moore <k.james.moore@gmail.com>
2017-12-15 15:22:48 -0500

    Scraping that sweet data.

--- scrape.py
+++ scrape.py
@@ -1,3 +1,21 @@
 # will need parsers at some point
 
-import 
+import requests
+from bs4 import BeautifulSoup
+
+
+STACK_OVERFLOW_URL = "https://stackoverflow.com/questions/tagged/python?sort=votes&pageSize=15"
+
+
+
+result = requests.get(STACK_OVERFLOW_URL)
+
+so_content = result.content
+
+soup = BeautifulSoup(so_content)
+
+samples = soup.find_all("a", "question-hyperlink")
+
+for s in range(len(samples)):
+    print(s)
+    print(samples[s])
