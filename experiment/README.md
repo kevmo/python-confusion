875664d352421b1b240fb7b3104a2be3a0f5e21d
Kevin Moore <k.james.moore@gmail.com>
2018-01-17 16:15:08 -0500

    WIP: Scraping Stack Overflow.  In middle of BS4 parsing/cleansing/munging

---
+++ experiment/README.md
@@ -0,0 +1,9 @@
+This is a hacking attempt at a word cloud.
+
+`mkvirtualenv confusion`
+
+or if you're already made the virtual env:
+`workon confusion`
+
+bash word-cloud.sh
+
--- experiment/scrape.py
+++ experiment/scrape.py
@@ -1,4 +1,4 @@
-# will need parsers at some point
+import json
 
 import requests
 from bs4 import BeautifulSoup
@@ -6,16 +6,25 @@ from bs4 import BeautifulSoup
 
 STACK_OVERFLOW_URL = "https://stackoverflow.com/questions/tagged/python?sort=votes&pageSize=15"
 
+PARSER = 'html.parser'
+
 
 
 result = requests.get(STACK_OVERFLOW_URL)
 
 so_content = result.content
 
-soup = BeautifulSoup(so_content)
+soup = BeautifulSoup(so_content, PARSER)
 
 samples = soup.find_all("a", "question-hyperlink")
 
-for s in range(len(samples)):
-    print(s)
-    print(samples[s])
+print(type(samples))
+
+# for s in range(len(samples)):
+#     print(s)
+#     print(samples[s])
+
+
+titles_JSON = json.load(samples)
+
+print(titles_JSON)
